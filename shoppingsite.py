4649cb6df54e53f64cf67e4127fe4f122d796e68
Hackbright Student <info@hackbrightacademy.com>
2016-07-21 11:58:04 -0700

    pushing

--- shoppingsite.py
+++ shoppingsite.py
@@ -7,7 +7,7 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken.
 """
 
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -50,7 +50,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id(59)
+    melon = melons.get_by_id(melon_id) # removed 59
     print melon
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -61,6 +61,7 @@ def shopping_cart():
     """Display content of shopping cart."""
 
     # TODO: Display the contents of the shopping cart.
+
 
     # The logic here will be something like:
     #
@@ -82,6 +83,9 @@ def add_to_cart(id):
     page and display a confirmation message: 'Successfully added to cart'.
     """
 
+    # session[melon_id] = []
+
+    
     # TODO: Finish shopping cart functionality
 
     # The logic here should be something like:
